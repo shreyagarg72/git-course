9079eb7251272524c435221ac6bfca1e8a804c9c
Shreya Garg <shreyag.garg72@gmail.com>
2024-02-23 16:19:12 +0530

    commit-2

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ function newFeatureAddB(){
     console.log("new feature added again b")
 
 }
+function newFeatureAddC(){
+    console.log("new feature added")
+    console.log("new feature added again C")
+
+}
